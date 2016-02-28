a73d2267ccffdc8caae5fa5cc69a91f296277ed8
Thu Hang <boongcd@yahoo.com>
2016-02-28 18:56:17 +0800

    Fixed bugs

--- integration_openMPI.c
+++ integration_openMPI.c
@@ -2,12 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <mpi.h>
-
+#include <math.h>
 int slaves;
 int myid;
-long strips;
-long result;
-long finalResult;
+long strips = 1000;
+int nprocs;
 
 long getUpperBound(double x)
 {
@@ -30,33 +29,58 @@ void slave_computation(double start, long iterations)
 {	
 	int i;
 	double size = 1.0/strips;
-	double upper=0;
-	double lower=0;
-	for (i=0; i<iteration; i++)
+	long upper=0;
+	long lower=0;
+	double xleft, xright;
+	long sum[2];
+	printf("size: %f, start: %f, id: %d \n", size, start, myid);
+	for (i=0; i<iterations; i++)
 	{
 		xleft=start+i*size;
 		xright=start+(i+1)*size;
 		upper+=getUpperBound(xleft);
 		lower+=getLowerBound(xright);
 	}
-	
+	MPI_Scan(&upper,&sum[0],1,MPI_LONG,MPI_SUM,MPI_COMM_WORLD);
+	MPI_Scan(&lower,&sum[1],1,MPI_LONG,MPI_SUM,MPI_COMM_WORLD);
+	//printf("upper: %li, lower: %li, id: %d \n", upper, lower, myid);
+	if(myid == nprocs-1)
+	{
+		printf("Sumupper: %li, Sumlower: %li \n", sum[0], sum[1]);
+	}
 }
 
-void slave(myId)
+void slave()
 {
 	long strips_per_slave = strips/slaves;
-	int extra_strips = strips_per_slave % slaves;
+	printf("%li \n", strips_per_slave);
+	int extra_strips = strips % slaves;
+	printf("%d \n", extra_strips);
 	double size = 1.0/strips;
 	double start=0;
-	if (myID <= extra_strips)
+	if (myid < extra_strips)
 	{	
 		//do 1 extra iteration
-		start = myId*size*(strips_per_slave+1);
+		printf("strips_per_slave: %li, \n", strips_per_slave);
+		start = myid*size*(strips_per_slave+1);
 		slave_computation(start, strips_per_slave+1);
 	}
 	else 
 	{
-		start = myId*size*strips_per_slave + extra_strips;
+		start = myid*size*strips_per_slave + extra_strips*size;
 		slave_computation(start, strips_per_slave);
 	}
 }
+
+int main(int argc, char **argv)
+{
+	MPI_Init(&argc,&argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+	
+	slaves = nprocs;
+	slave();
+	MPI_Finalize();
+	return 0;
+}
+
