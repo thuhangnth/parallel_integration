b9005473cd94f5e29ff6f2df6a847389f4b826ec
Thu Hang <boongcd@yahoo.com>
2016-03-03 18:28:18 +0800

    For Submission

--- integration_openMP.c
+++ integration_openMP.c
@@ -6,60 +6,60 @@
 
 long long wall_clock_time()
 {
-        struct timespec tp;
-        clock_gettime(CLOCK_REALTIME, &tp);
-        return (long long)(tp.tv_nsec + (long long)tp.tv_sec*1000000000ll);
+	struct timespec tp;
+	clock_gettime(CLOCK_REALTIME, &tp);
+	return (long long)(tp.tv_nsec + (long long)tp.tv_sec*1000000000ll);
 }
 
 long getUpperBound(double x, double scale)
 {
-        float y;
-        y = 4.0/(1+pow(x,2))*scale;
-        if (y-(long)y > 0)
-                y = (long)y+1;
-        return (long)y;
+	float y;
+	y = 4.0/(1+pow(x,2))*scale;
+	if (y-(long)y > 0)
+       		y = (long)y+1;
+	return (long)y;
 }
 
 long getLowerBound(double x, double scale)
 {
-        float y;
-        y = 4.0/(1+pow(x,2))*scale;
-        return (long)y;
-  
-½nt main(int argc, char **argv)
+	float y;
+	y = 4.0/(1+pow(x,2))*scale;
+	return (long)y;
+}
+int main(int argc, char **argv)
 {
-        long n;
-        int i;
-        double size;
-        double xleft =0;
-        double xright=0;
-        long sumUpper=0;
-        long sumLower=0;
-        long long before, after;
-        if (argc >=2)
-                n = atol(argv[1]);                                                                                                                                           
-        else                                                                                                                                                                 
-                n = 10;                                                                                                                                                      
-                                                                                                                                                                             
-        before = wall_clock_time();                                                                                                                                          
-        size = 1.0/n;                                                                                                                                                        
-        #pragma omp parallel for private (xleft, xright) reduction (+:sumUpper,sumLower)                                                                                     
-        for (i=0; i<n; i++)                                                                                                                                                  
-        {                                                                                                                                                                    
-                xleft = (double)i*size;                                                                                                                                      
-                xright=(double)(i+1)*size;                                                                                                                                   
-                sumUpper+= getUpperBound(xleft, n);                                                                                                                          
-                sumLower+= getLowerBound(xright, n);                                                                                                                         
-        }                                                                                                                                                                    
-                                                                                                                                                                             
-        double result= (double)(sumUpper+sumLower)/2.0*(size*size);                                                                                                          
-        after = wall_clock_time();                                                                                                                                           
-        printf("result: %.8lf \n", result);                                                                                                                                  
-        int threads;                                                                                                                                                         
-        #pragma omp parallel
-        {
-        threads = omp_get_num_threads();                                                                                                                                     
-        }
-        printf("The integration took %2.4f seconds using %d threads and %li strips \n", ((float)(after-before))/1000000000,threads,n);
-        return 0;                                             
+	long n;
+	int i;
+	double size;
+	double xleft =0;
+	double xright=0;
+	long sumUpper=0;
+	long sumLower=0;
+	long long before, after;
+	if (argc >=2)
+		n = atol(argv[1]);
+	else
+		n = 10;
+
+	before = wall_clock_time();
+	size = 1.0/n;
+	#pragma omp parallel for private (xleft, xright) reduction (+:sumUpper,sumLower)
+	for (i=0; i<n; i++)
+	{
+		xleft = (double)i*size;
+		xright=(double)(i+1)*size;
+		sumUpper+= getUpperBound(xleft, n);
+		sumLower+= getLowerBound(xright, n);	
+	}
+	
+	double result= (double)(sumUpper+sumLower)/2.0*(size*size);
+	after = wall_clock_time();
+	printf("result: %.8lf \n", result);
+	int threads;
+	#pragma omp parallel
+	{
+	threads = omp_get_num_threads();
+	}
+	printf("The integration took %2.4f seconds using %d threads and %li strips \n", ((float)(after-before))/1000000000,threads,n);
+	return 0;	
 }
--- integration_openMPI.c
+++ integration_openMPI.c
@@ -3,10 +3,19 @@
 #include <time.h>
 #include <mpi.h>
 #include <math.h>
+
 int slaves;
 int myid;
-long strips = 1000;
+long strips;
 int nprocs;
+long long runtime;
+
+long long wall_clock_time()
+{
+	struct timespec tp;
+	clock_gettime(CLOCK_REALTIME,&tp);
+	return (long long)(tp.tv_nsec + (long long)tp.tv_sec*1000000000ll);
+}
 
 long getUpperBound(double x)
 {
@@ -29,39 +38,36 @@ void slave_computation(double start, long iterations)
 {	
 	int i;
 	double size = 1.0/strips;
-	long upper=0;
-	long lower=0;
+	long localSum[2]={0,0};
 	double xleft, xright;
-	long sum[2];
-	printf("size: %f, start: %f, id: %d \n", size, start, myid);
+	long sum[2]={0,0};
 	for (i=0; i<iterations; i++)
 	{
 		xleft=start+i*size;
 		xright=start+(i+1)*size;
-		upper+=getUpperBound(xleft);
-		lower+=getLowerBound(xright);
+		localSum[0]+=getUpperBound(xleft);
+		localSum[1]+=getLowerBound(xright);
 	}
-	MPI_Scan(&upper,&sum[0],1,MPI_LONG,MPI_SUM,MPI_COMM_WORLD);
-	MPI_Scan(&lower,&sum[1],1,MPI_LONG,MPI_SUM,MPI_COMM_WORLD);
-	//printf("upper: %li, lower: %li, id: %d \n", upper, lower, myid);
+	MPI_Reduce(&localSum,&sum,2,MPI_LONG,MPI_SUM,nprocs-1,MPI_COMM_WORLD);
+	//printf("id: %d, sum: %li \n", myid,sum[0]);
 	if(myid == nprocs-1)
 	{
-		printf("Sumupper: %li, Sumlower: %li \n", sum[0], sum[1]);
+		double result = (sum[0]+sum[1])/2.0*(size*size);
+		printf("Integration result: %.8f", result);
 	}
 }
 
 void slave()
 {
 	long strips_per_slave = strips/slaves;
-	printf("%li \n", strips_per_slave);
 	int extra_strips = strips % slaves;
-	printf("%d \n", extra_strips);
 	double size = 1.0/strips;
 	double start=0;
+	long long before, after;
+	before = wall_clock_time();
 	if (myid < extra_strips)
 	{	
 		//do 1 extra iteration
-		printf("strips_per_slave: %li, \n", strips_per_slave);
 		start = myid*size*(strips_per_slave+1);
 		slave_computation(start, strips_per_slave+1);
 	}
@@ -70,6 +76,12 @@ void slave()
 		start = myid*size*strips_per_slave + extra_strips*size;
 		slave_computation(start, strips_per_slave);
 	}
+	after = wall_clock_time();
+	runtime += after - before;
+	if (myid == nprocs -1)
+	{
+		printf(",took %.3f seconds using %li strips and %d threads \n",runtime/1000000000.0, strips, nprocs);
+	}
 }
 
 int main(int argc, char **argv)
@@ -77,7 +89,14 @@ int main(int argc, char **argv)
 	MPI_Init(&argc,&argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
-	
+	if(argc>=2)
+	{
+		strips = atol(argv[1]);
+	}
+	else
+	{	
+		strips = 10;
+	}	
 	slaves = nprocs;
 	slave();
 	MPI_Finalize();
